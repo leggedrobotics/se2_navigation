55cd26eb67b0355da3c1806d05e3063a1407ba24
jelavice <edo.jelavic@gmail.com>
2020-03-21 00:35:25 +0100

    Add Ackermann steering controller

---
+++ pure_pursuit/include/pure_pursuit/heading_control/AckermannSteeringController.hpp
@@ -0,0 +1,23 @@
+/*
+ * AckermannSteeringController.hpp
+ *
+ *  Created on: Mar 21, 2020
+ *      Author: jelavice
+ */
+
+#pragma once
+
+#include "pure_pursuit/heading_control/HeadingController.hpp"
+
+namespace pure_pursuit {
+
+class AckermannSteeringController : public HeadingController {
+ public:
+  AckermannSteeringController() = default;
+  ~AckermannSteeringController() override = default;
+
+ private:
+  bool runController(double dt) override;
+};
+
+}  // namespace pure_pursuit
---
+++ pure_pursuit/src/AckermannSteeringController.cpp
@@ -0,0 +1,17 @@
+/*
+ * AckermannSteeringController.cpp
+ *
+ *  Created on: Mar 21, 2020
+ *      Author: jelavice
+ */
+
+#include "pure_pursuit/heading_control/AckermannSteeringController.hpp"
+
+namespace pure_pursuit {
+
+bool AckermannSteeringController::runController(double dt) {
+  // todo implement
+  return true;
+}
+
+} /* namespace pure_pursuit */
