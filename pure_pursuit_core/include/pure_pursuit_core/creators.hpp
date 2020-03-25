e4b48a77248662620b62e8883de6c73c4670fac6
jelavice <edo.jelavic@gmail.com>
2020-03-25 17:26:54 +0100

    Add creators

---
+++ pure_pursuit_core/include/pure_pursuit_core/creators.hpp
@@ -0,0 +1,31 @@
+/*
+ * creators.hpp
+ *
+ *  Created on: Mar 25, 2020
+ *      Author: jelavice
+ */
+
+#pragma once
+
+#include <memory>
+
+namespace pure_pursuit {
+
+class LongitudinalVelocityController;
+class HeadingController;
+class AckermannSteeringCtrlParameters;
+class PathPreprocessor;
+class ProgressValidator;
+class PathTracker;
+
+std::unique_ptr<LongitudinalVelocityController> createConstantVelocityController(double desiredConstantVelocity);
+std::unique_ptr<HeadingController> createAckermannSteeringController(const AckermannSteeringCtrlParameters& parameters);
+std::unique_ptr<PathPreprocessor> createPathPreprocessor(double minimumSegmentLength);
+std::unique_ptr<ProgressValidator> createProgressValidator(double goalDistanceTolerance);
+std::unique_ptr<PathTracker> createSimplePathTracker(double waitingTimeBetweenTurns,
+                                                     std::shared_ptr<LongitudinalVelocityController> velocityController,
+                                                     std::shared_ptr<HeadingController> headingController,
+                                                     std::shared_ptr<ProgressValidator> validator,
+                                                     std::shared_ptr<PathPreprocessor> pathPreprocessor);
+
+} /* namespace pure_pursuit */
---
+++ pure_pursuit_core/src/creators.cpp
@@ -0,0 +1,56 @@
+/*
+ * creators.cpp
+ *
+ *  Created on: Mar 25, 2020
+ *      Author: jelavice
+ */
+
+#include "pure_pursuit_core/creators.hpp"
+#include "pure_pursuit_core/heading_control/AckermannSteeringController.hpp"
+#include "pure_pursuit_core/path_tracking/PathPreprocessor.hpp"
+#include "pure_pursuit_core/path_tracking/ProgressValidator.hpp"
+#include "pure_pursuit_core/path_tracking/SimplePathTracker.hpp"
+#include "pure_pursuit_core/velocity_control/ConstantVelocityController.hpp"
+
+namespace pure_pursuit {
+
+std::unique_ptr<LongitudinalVelocityController> createConstantVelocityController(double desiredConstantVelocity) {
+  std::unique_ptr<ConstantVelocityController> controller = std::make_unique<ConstantVelocityController>();
+  controller->setDesiredVelocity(desiredConstantVelocity);
+  return std::move(controller);
+}
+
+std::unique_ptr<HeadingController> createAckermannSteeringController(const AckermannSteeringCtrlParameters& parameters) {
+  std::unique_ptr<AckermannSteeringController> controller = std::make_unique<AckermannSteeringController>();
+  controller->setParameters(parameters);
+  return std::move(controller);
+}
+
+std::unique_ptr<PathPreprocessor> createPathPreprocessor(double minimumSegmentLength) {
+  std::unique_ptr<PathPreprocessor> preprocessor = std::make_unique<PathPreprocessor>();
+  preprocessor->setMinimumSegmentLength(minimumSegmentLength);
+  return std::move(preprocessor);
+}
+
+std::unique_ptr<ProgressValidator> createProgressValidator(double goalDistanceTolerance) {
+  std::unique_ptr<ProgressValidator> validator = std::make_unique<ProgressValidator>();
+  validator->setGoalDistanceTolerance(goalDistanceTolerance);
+  return std::move(validator);
+}
+
+std::unique_ptr<PathTracker> createSimplePathTracker(double waitingTimeBetweenTurns,
+                                                     std::shared_ptr<LongitudinalVelocityController> velocityController,
+                                                     std::shared_ptr<HeadingController> headingController,
+                                                     std::shared_ptr<ProgressValidator> validator,
+                                                     std::shared_ptr<PathPreprocessor> pathPreprocessor) {
+  std::unique_ptr<SimplePathTracker> tracker = std::make_unique<SimplePathTracker>();
+  tracker->setWaitingTimeBetweenDirectionSwitches(waitingTimeBetweenTurns);
+  tracker->setHeadingController(headingController);
+  tracker->setVelocityController(velocityController);
+  tracker->setProgressValidator(validator);
+  tracker->setPathPreprocessor(pathPreprocessor);
+  return std::move(tracker);
+}
+
+}  // namespace pure_pursuit
+/* namespace pure_pursuit */
