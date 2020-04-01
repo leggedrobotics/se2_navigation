8232cf9397b35fe9106242acd3752efc9321faa8
jelavice <edo.jelavic@gmail.com>
2020-04-01 17:44:44 +0200

    Add ros node

---
+++ se2_planning/src/se2_planner_node.cpp
@@ -0,0 +1,20 @@
+/*
+ * se2_planner_node.cpp
+ *
+ *  Created on: Apr 1, 2020
+ *      Author: jelavice
+ */
+
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "se2_planner_node");
+  ros::NodeHandle nh;
+
+
+  ros::spin();
+
+  return 0;
+}
