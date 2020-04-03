6eedf3a11365e5a29d2e8f7cc650e1f5ebb89d0b
jelavice <edo.jelavic@gmail.com>
2020-04-03 14:17:28 +0200

    split into ros and non-ros

---
+++ se2_planning_ros/src/se2_planner_node.cpp
@@ -0,0 +1,34 @@
+/*
+ * se2_planner_node.cpp
+ *
+ *  Created on: Apr 1, 2020
+ *      Author: jelavice
+ */
+
+#include <ros/ros.h>
+
+#include "se2_planning/OmplReedsSheppPlannerRos.hpp"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "se2_planner_node");
+  ros::NodeHandle nh;
+
+  se2_planning::OmplReedsSheppPlannerRos planner(&nh);
+  //  se2_planning::OmplReedsSheppPlanner planner;
+  planner.initialize();
+  using namespace se2_planning;
+  ReedsSheppState start, goal;
+  goal.x_ = 0.0;
+  goal.y_ = 10.0;
+  planner.setStartingState(start);
+  planner.setGoalState(goal);
+  planner.plan();
+  ReedsSheppPath path;
+  planner.getPath(&path);
+  std::cout << "Planned path: " << std::endl;
+  std::cout << path << std::endl;
+
+  ros::spin();
+
+  return 0;
+}
