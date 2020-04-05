9668e6616f8e0322733ed43cd447803a2f3b0a56
jelavice <edo.jelavic@gmail.com>
2020-04-05 22:44:23 +0200

    Add the messages

---
+++ se2_planning_ros/include/se2_planning_ros/PlannerRos.hpp
@@ -0,0 +1,44 @@
+/*
+ * PlannerRos.hpp
+ *
+ *  Created on: Apr 5, 2020
+ *      Author: jelavice
+ */
+
+#pragma once
+
+#include "se2_planning/Planner.hpp"
+#include <ros/ros.h>
+#include "se2_navigation_msgs/PathRequestSrv.h"
+
+namespace se2_planning {
+
+class PlannerRos : public Planner
+{
+
+ public:
+  void setStartingState(const State& startingState) override;
+  void setGoalState(const State& goalState) override;
+  bool plan() override;
+  void getPath(Path* path) const override;
+  bool reset() override;
+  bool initialize() override;
+  void getStartingState(State* startingState) override;
+  void getGoalState(State* goalState) override;
+
+  void setPlanner(std::unique_ptr<Planner> planner);
+ protected:
+
+  using PlanningService = se2_navigation_msgs::PathRequestSrv;
+
+  explicit PlannerRos(ros::NodeHandlePtr nh);
+  ~PlannerRos() override = default;
+  virtual void initializeRos() = 0;
+  virtual bool planningService(PlanningService::Request &req, PlanningService::Response &res) = 0;
+
+  ros::NodeHandlePtr nh_;
+  std::unique_ptr<Planner> planner_;
+
+};
+
+} /* namespace se2_planning*/
---
+++ se2_planning_ros/src/PlannerRos.cpp
@@ -0,0 +1,56 @@
+/*
+ * PlannerRos.cpp
+ *
+ *  Created on: Apr 5, 2020
+ *      Author: jelavice
+ */
+
+#include "se2_planning_ros/PlannerRos.hpp"
+
+namespace se2_planning {
+
+PlannerRos::PlannerRos(ros::NodeHandlePtr nh)
+    : nh_(nh)
+{
+  initializeRos();
+}
+
+void PlannerRos::setPlanner(std::unique_ptr<Planner> planner)
+{
+  planner_ = std::move(planner);
+}
+
+void PlannerRos::setStartingState(const State& startingState)
+{
+  planner_->setStartingState(startingState);
+}
+void PlannerRos::setGoalState(const State& goalState)
+{
+  planner_->setGoalState(goalState);
+}
+bool PlannerRos::plan()
+{
+ return planner_->plan();
+}
+void PlannerRos::getPath(Path* path) const
+{
+  planner_->getPath(path);
+}
+bool PlannerRos::reset()
+{
+  return planner_->reset();
+}
+bool PlannerRos::initialize()
+{
+  return planner_->initialize();
+}
+void PlannerRos::getStartingState(State* startingState)
+{
+  planner_->getStartingState(startingState);
+}
+void PlannerRos::getGoalState(State* goalState)
+{
+  planner_->getGoalState(goalState);
+}
+
+} /* namespace planner*/
