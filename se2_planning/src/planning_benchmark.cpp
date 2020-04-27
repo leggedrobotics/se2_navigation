a0857d70406c033f0dfc7d2b86b0473da9ef8c48
jelavice <edo.jelavic@gmail.com>
2020-04-27 12:25:41 +0200

    Cleanup benchmark

--- se2_planning/src/planning_benchmark.cpp
+++ se2_planning/src/planning_benchmark.cpp
@@ -16,6 +16,9 @@
 
 #define duration(a) std::chrono::duration_cast<std::chrono::milliseconds>(a).count()
 
+const int numTestCases = 500;
+const std::string benchmarkLayer = "occupancy";
+
 void setCostThreshold(se2_planning::OmplReedsSheppPlanner* planner) {
   auto si = planner->getSimpleSetup()->getSpaceInformation();
   ompl::base::OptimizationObjectivePtr optimizationObjective(std::make_shared<ompl::base::PathLengthOptimizationObjective>(si));
@@ -28,6 +31,43 @@ void createRectangularStateSpace(double stateBound, se2_planning::OmplReedsShepp
   parameters->xUpperBound_ = stateBound;
   parameters->yLowerBound_ = -stateBound;
   parameters->yUpperBound_ = stateBound;
+}
+
+void runPlanner(se2_planning::OmplReedsSheppPlanner& planner) {
+  const se2_planning::ReedsSheppState start(0.0, -10.0, 0.0);
+  const se2_planning::ReedsSheppState goal(0.0, 10.0, 0.0);
+
+  int count = 0;
+  auto tStart = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < numTestCases; ++i) {
+    planner.setStartingState(start);
+    planner.setGoalState(goal);
+    if (planner.plan()) {
+      count++;
+    }
+  }
+  auto tFinish = std::chrono::high_resolution_clock::now();
+  std::cout << "Done, success rate: " << count << "/" << numTestCases << std::endl;
+  std::cout << "total duration: " << duration(tFinish - tStart) << " miliseconds" << std::endl;
+  std::cout << "per planning query: " << static_cast<double>(duration(tFinish - tStart)) / numTestCases << " miliseconds" << std::endl;
+}
+
+void runNormalValidator(se2_planning::OmplReedsSheppPlanner& planner, const grid_map::GridMap& gridMap) {
+  // create state validator
+  auto plannerStateValidator =
+      se2_planning::createGridMapStateValidator(gridMap, se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), benchmarkLayer);
+  planner.setStateValidator(std::move(plannerStateValidator));
+  std::cout << "benchmarking standard validator " << std::endl;
+  runPlanner(planner);
+}
+
+void runlazylValidator(se2_planning::OmplReedsSheppPlanner& planner, const grid_map::GridMap& gridMap) {
+  // create state validator
+  auto plannerStateValidator =
+      se2_planning::createGridMapLazyStateValidator(gridMap, se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), benchmarkLayer);
+  planner.setStateValidator(std::move(plannerStateValidator));
+  std::cout << "benchmarking lazy validator " << std::endl;
+  runPlanner(planner);
 }
 
 int main(int argc, char** argv) {
@@ -43,15 +83,6 @@ int main(int argc, char** argv) {
   };
   test::addObstacles(isAnObstacle, testLayer, &gridMap);
 
-  // create state validator
-  //  auto plannerStateValidator =
-  //      se2_planning::createGridMapStateValidator(gridMap, se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), testLayer);
-  //  se2_planning::GridMapStateValidator validator = *plannerStateValidator;
-
-  auto plannerStateValidator =
-      se2_planning::createGridMapLazyStateValidator(gridMap, se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), testLayer);
-  se2_planning::GridMapLazyStateValidator validator = *plannerStateValidator;
-
   // setup planner
   se2_planning::OmplReedsSheppPlannerParameters parameters;
   parameters.maxPlanningTime_ = 10.0;
@@ -61,28 +92,11 @@ int main(int argc, char** argv) {
   se2_planning::OmplReedsSheppPlanner planner;
   planner.setParameters(parameters);
   planner.initialize();
-  planner.setStateValidator(std::move(plannerStateValidator));
   setCostThreshold(&planner);
 
-  const se2_planning::ReedsSheppState start(0.0, -10.0, 0.0);
-  const se2_planning::ReedsSheppState goal(0.0, 10.0, 0.0);
+  runNormalValidator(planner, gridMap);
+  std::cout << "\n===================\n";
+  runlazylValidator(planner, gridMap);
 
-  std::cout << "gonna start benchmarking now " << std::endl;
-  std::cout << "Press enter key to continue..." << std::endl;
-  getchar();
-  const int numTestCases = 1000;
-  int count = 0;
-  auto tStart = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < numTestCases; ++i) {
-    planner.setStartingState(start);
-    planner.setGoalState(goal);
-    if (planner.plan()) {
-      count++;
-    }
-  }
-  auto tFinish = std::chrono::high_resolution_clock::now();
-  std::cout << "Done, success rate: " << count << "/" << numTestCases << std::endl;
-  std::cout << "total duration: " << duration(tFinish - tStart) << " miliseconds" << std::endl;
-  std::cout << "per planning query: " << static_cast<double>(duration(tFinish - tStart)) / numTestCases << " miliseconds" << std::endl;
   return 0;
 }
